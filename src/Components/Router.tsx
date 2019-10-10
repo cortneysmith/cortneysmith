7d0d5b66294ca8f5a12fb216aecb997685f5c46e
Cortney Wood <woodcortney@gmail.com>
2019-10-09 19:39:40 -0500

    actually fixed routing? maybe??

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Blog from "./Blog";
 import BlogPost from "./BlogPost";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/blog" component={Blog} />
       <Route exact path="/blog/posts/:post" component={BlogPost} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
